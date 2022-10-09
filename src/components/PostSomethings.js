2cb7f9cff7930cc5037d89b9f1ac2a6cca8e4703
syab-syab <syabsyabsyabsyab@gmail.com>
2022-10-09 13:50:02 +0900

    ルーティングとコンポーネント少しだけ

---
+++ src/components/PostSomethings.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function PostSomethings() {
+  return (
+    <div>PostSomethings</div>
+  )
+}
--- src/components/PostTeas.js
+++ src/components/PostTeas.js
@@ -1,37 +1,9 @@
-import './App.css';
-import { useState } from 'react';
-import { Routes, Route  } from 'react-router-dom';
-import Home from './components/Home';
-import PostTeas from './components/PostTeas';
-import PostSomethings from './components/PostSomethings';
+import React from 'react'
 
-function App() {
-
-  const [teaName, setTeaName] = useState('test');
-
-  const [indicateValue, setIndicateValue] = useState('');
-
-  const handleChange = (e) => {
-    setTeaName(e.target.value);
-  }
-
-  const handleClick = (e) => {
-    setIndicateValue(teaName);
-    setTeaName('');
-  } 
-
+export default function PostTeas() {
   return (
-    <div className="App">
-      <header>
-        <p>Railsで作ったAPIからデータのやり取りをするテスト</p>
-      </header>
-      <main>
-        <Routes path='/'>
-          <Route path='/' element={<Home />} />
-          <Route path='/post-teas' element={<PostTeas />} />
-          <Route path='/post-somethings' element={<PostSomethings />}
-        </Routes>
-        <section>
+    <div>
+      <p>PostTeas</p>
         {/* 各input要素を分けた方が良いかも */}
         <form>
           <label>
@@ -84,10 +56,7 @@ function App() {
         </form>
         <p>{indicateValue}</p>
 
-        </section>
-      </main>
     </div>
-  );
-}
 
-export default App;
+  )
+}
